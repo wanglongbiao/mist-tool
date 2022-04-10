8b7fcab933192cbee2a5aab74d51621e70488c01
wanglongbiao <wanglongbiao@vip.qq.com>
2022-04-10 15:26:46 +0800

    save location

--- front-tool/src/conponents/OlMap/index.jsx
+++ front-tool/src/conponents/OlMap/index.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
 import * as ol from 'ol'
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
+import { OSM, TileDebug, XYZ, Vector as VectorSource } from 'ol/source'
+import TileLayer from 'ol/layer/Tile'
+import { fromLonLat, toLonLat } from 'ol/proj'
+import MousePosition from 'ol/control/MousePosition'
+import { toStringXY, toStringHDMS } from 'ol/coordinate'
+import { ScaleLine } from 'ol/control';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import { Circle } from 'ol/geom';
+import WKT from 'ol/format/WKT';
 import 'ol/ol.css';
 import './index.css'
-
 export default class OlMap extends Component {
     constructor(props) {
         super(props)
-        let center = localStorage.getItem('center')
-        let zoom = localStorage.getItem('zoom')
-        this.state = { center: [0,0], zoom: zoom == undefined ? 5 : zoom }
+        let center = localStorage.center ? localStorage.center.split(',') : [130, 20]
+        let zoom = localStorage.zoom ? localStorage.zoom : 3
+        this.state = { center: center, zoom: zoom }
+        let vectorSource = new VectorSource()
+        let osmTileLayer = new TileLayer({
+            source: new OSM(),
+        })
+        let debugTileLayer = new TileLayer({
+            source: new TileDebug(),
+        })
+        let xyzTileLayer = new TileLayer({
+            source: new XYZ({ url: 'http://localhost/land-map/{z}/{x}/{y}.png' }),
+        })
         this.map = new ol.Map({
             layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
+                osmTileLayer,
+                debugTileLayer
             ],
             target: null,
             view: new ol.View({
-                center: this.state.center,
+                center: fromLonLat(this.state.center),
                 zoom: this.state.zoom,
             }),
         });
@@ -28,7 +45,7 @@ export default class OlMap extends Component {
         console.log('render..');
         return (
             <div> Ol map
-                <div id="map" className="map" tabIndex="0"></div>
+                <div id="map" className="map"></div>
             </div>
         )
     }
@@ -36,5 +53,22 @@ export default class OlMap extends Component {
     componentDidMount() {
         console.log('componentDidMount..');
         this.map.setTarget('map')
+
+        // 显示经纬度
+        var mousePositionControl = new MousePosition({
+            coordinateFormat: (c) => toStringXY(c, 6),
+            projection: 'EPSG:4326',
+            className: 'custom-mouse-position',
+            target: 'mouse-position',
+            undefinedHTML: ''
+        })
+        this.map.addControl(mousePositionControl)
+        // 记住最后位置和缩放级别
+        this.map.on('moveend', function (e) {
+            localStorage.zoom = e.target.getView().getZoom()
+            localStorage.center = toLonLat(e.target.getView().getCenter())
+            // vectorSource.clear()
+            // vectorSource.addFeature(new Feature(new Point(map.getView().getCenter())))
+        })
     }
 }
