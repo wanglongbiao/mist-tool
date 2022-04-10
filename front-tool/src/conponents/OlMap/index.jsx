e44374557e5aa15db8906cfe809f4c304c35e70f
wanglongbiao <wanglongbiao@vip.qq.com>
2022-04-10 15:41:42 +0800

    add wkt

--- front-tool/src/conponents/OlMap/index.jsx
+++ front-tool/src/conponents/OlMap/index.jsx
@@ -19,20 +19,23 @@ export default class OlMap extends Component {
         let center = localStorage.center ? localStorage.center.split(',') : [130, 20]
         let zoom = localStorage.zoom ? localStorage.zoom : 3
         this.state = { center: center, zoom: zoom }
-        let vectorSource = new VectorSource()
         let osmTileLayer = new TileLayer({
             source: new OSM(),
         })
-        let debugTileLayer = new TileLayer({
+        let debugLayer = new TileLayer({
             source: new TileDebug(),
         })
         let xyzTileLayer = new TileLayer({
             source: new XYZ({ url: 'http://localhost/land-map/{z}/{x}/{y}.png' }),
         })
+        this.vectorLayer = new VectorLayer({
+            source: new VectorSource()
+        })
         this.map = new ol.Map({
             layers: [
                 osmTileLayer,
-                debugTileLayer
+                this.vectorLayer,
+                debugLayer
             ],
             target: null,
             view: new ol.View({
@@ -70,5 +73,24 @@ export default class OlMap extends Component {
             // vectorSource.clear()
             // vectorSource.addFeature(new Feature(new Point(map.getView().getCenter())))
         })
+        // 添加比例尺
+        this.map.addControl(new ScaleLine({ units: 'metric' }))
+
+        // 添加原点
+        // vectorSource.addFeature(new Feature(new Point([0, 0])))
+        // vectorSource.addFeature(new Feature(new Point(fromLonLat([120.317777, 26.948889]))))
+        // vectorSource.addFeature(new Feature(new Point(fromLonLat([120.318, 26.948889]))))
+        this.addWkt()
+    }
+
+    addWkt = wkt => {
+        let format = new WKT()
+        let option = {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+        }
+        let wkt1 = 'POLYGON ((108.91296386718746 18.2552737236136, 109.09835815429683 18.2927646679165, 109.1457366943359 18.198532912099026, 108.99261474609371 18.14079485121475, 108.86936187744138 18.116323816056436, 108.91296386718746 18.2552737236136))'
+        let feature = format.readFeature(wkt1, option)
+        this.vectorLayer.getSource().addFeature(feature)
     }
 }
