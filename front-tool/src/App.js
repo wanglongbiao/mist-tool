aeaa34746aa3297f1b8838623583e735cf3754da
wanglongbiao <wanglongbiao@vip.qq.com>
2022-04-17 08:54:20 +0800

    mynav link

--- front-tool/src/App.js
+++ front-tool/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import MyNavLink from './components/MyNavLink'
 import About from './pages/About'
 import Home from './pages/Home'
 
@@ -9,12 +10,14 @@ export default class App extends Component {
       <div className='row'>
         <BrowserRouter>
           <div>
+            <MyNavLink to='/home2'>MyNavLink</MyNavLink>
             <NavLink className='list-group-item' to='/home'>Home</NavLink>
             <NavLink className='list-group-item' to='/about'>About</NavLink>
           </div>
           <div>
             <Routes>
               <Route path='/home' element={<Home />}></Route>
+              <Route path='/home2' element={<Home />}></Route>
               <Route path='/about' element={<About />}></Route>
             </Routes>
           </div>
